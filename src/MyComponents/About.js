b672ebe26256fa3c5de97ea2e379b356d3d42320
Amaan Khan <amaank2077@gmail.com>
2023-02-04 15:20:37 +0530

    The initial addition of the project is done

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis odio, nesciunt sed eveniet, <br />
+                nemo amet, dignissimos magnam itaque ipsam possimus. Inventore vitae, perferendis harum quas cupiditate<br />
+                adipisci repellendus tempore asperiores at consequatur quos?
+            </p>
+        </div>
+    )
+}
